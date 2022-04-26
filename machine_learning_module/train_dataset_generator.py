48526efc828ea66a331bedeeecb30ef14d410d63
yagol <yhw_yagol@yeah.net>
2022-04-26 11:48:10 +0800

    add module: calculate weight sum and diff

--- machine_learning_module/train_dataset_generator.py
+++ machine_learning_module/train_dataset_generator.py
@@ -17,6 +17,7 @@ def gen_train_dataset_with_bits_array(max_feature_length=100):
     x_data = []
     y_data = []
     df = pd.read_csv(dataset_path)
+    longest_testcase_length = 0
     for index, row in df.iterrows():
         # 特征向量
         x = []
@@ -34,6 +35,8 @@ def gen_train_dataset_with_bits_array(max_feature_length=100):
         x.extend(p5)
         x.extend(p6)
         x.extend(p7)
+        if len(x) > longest_testcase_length:
+            longest_testcase_length = len(x)
         if len(x) > max_feature_length:
             x = x[:max_feature_length]
         else:
@@ -46,6 +49,7 @@ def gen_train_dataset_with_bits_array(max_feature_length=100):
         c4 = row['c4']
         c5 = row['c5']
         y_data.append([1 if i else 0 for i in [c1, c2, c3, c4, c5]])
+    print("longest_testcase_length:", longest_testcase_length)
     return x_data, y_data
 
 
@@ -53,6 +57,7 @@ def gen_train_dataset_with_bytes_array(max_feature_length=100):
     df = pd.read_csv(dataset_path)
     x_data = []
     y_data = []
+    longest_testcase_length = 0
     for index, row in df.iterrows():
         testcase_one_row = ""
         testcase_one_row += str(row['p1'])
@@ -63,6 +68,8 @@ def gen_train_dataset_with_bytes_array(max_feature_length=100):
         testcase_one_row += str(row['p6'])
         testcase_one_row += str(row['p7'])
         x = bytearray(testcase_one_row, 'utf-8')
+        if len(x) > longest_testcase_length:
+            longest_testcase_length = len(x)
         if len(x) > max_feature_length:
             x = x[:max_feature_length]
         else:
@@ -75,4 +82,5 @@ def gen_train_dataset_with_bytes_array(max_feature_length=100):
         c5 = row['c5']
         y = [1 if i else 0 for i in [c1, c2, c3, c4, c5]]
         y_data.append(y)
+    print("longest_testcase_length:", longest_testcase_length)
     return x_data, y_data
--- machine_learning_module/weight_diff_calculate.py
+++ machine_learning_module/weight_diff_calculate.py
@@ -9,9 +9,10 @@ def euclidean(point1, point2):
     return np.sqrt(diff1 ** 2 + diff2 ** 2 + diff3 ** 2)
 
 
-def method_name(feature_sizes, label_sizes, hidden_layer_sizes, clf):
+def calculate_weight_diff_for_each_output(feature_sizes, label_sizes, hidden_layer_sizes, clf, top_k=5):
     assert len(hidden_layer_sizes) == 2
     for i in range(label_sizes):
+        summary = []
         for j in range(feature_sizes):
             points = []
             sum_of_weight_diff = 0
@@ -27,4 +28,6 @@ def method_name(feature_sizes, label_sizes, hidden_layer_sizes, clf):
             for m in range(len(points)):
                 for n in range(m + 1, len(points)):
                     sum_of_weight_diff += euclidean(points[m], points[n])
-            print(f"{i}-{j}:{sum_of_weight_diff}")
+            summary.append((sum_of_weight_diff, j))
+        summary.sort(key=lambda x: x[0], reverse=True)
+        print(f"{i}:{summary[:top_k]}")
