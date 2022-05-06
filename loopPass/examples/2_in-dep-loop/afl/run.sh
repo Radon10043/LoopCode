4f08e82c35ea1b49435594a58e77285daf7aed45
Radon10043 <772577952@qq.com>
2022-05-06 20:57:48 +0800

    Modfy script

--- loopPass/examples/2_in-dep-loop/afl/run.sh
+++ loopPass/examples/2_in-dep-loop/afl/run.sh
@@ -1,15 +1,20 @@
+
 export AFL=~/Documents/project_vscode/cpp/llvm/5_LoopCode/afl
 
 if [ -d "temp/" ]; then
     rm -r temp
 fi
 
-mkdir temp; cp LoopBBs.txt temp/
+mkdir temp;
 
 cd ..
 export SUBJECT=$PWD; export TMP_DIR=$PWD/afl/temp
 export CC=$AFL/afl-clang-fast; export CXX=$AFL/afl-clang-fast++
+export ADDITIONAL="-outdir=$TMP_DIR"
 
 $CC loop.c $ADDITIONAL -g -o loop.out
-cd afl/temp; mkdir in; echo "" > in/in
+
+$CC loop.c -bbfile=$TMP_DIR/BBFile.txt -g -o loop.out
+
+cd afl/temp; mkdir in; echo "" > in/in; echo "" > in/in2
 $AFL/afl-fuzz -m none -i in -o out ../../loop.out
