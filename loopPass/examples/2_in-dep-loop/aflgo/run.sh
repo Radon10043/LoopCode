3d0b8dc3ad536f47da5368631a325027d5e5ac2f
Radon10043 <772577952@qq.com>
2022-05-20 18:01:06 +0800

    Update

--- loopPass/examples/2_in-dep-loop/aflgo/run.sh
+++ loopPass/examples/2_in-dep-loop/aflgo/run.sh
@@ -1,19 +1,28 @@
+
 export AFLGO=~/Documents/project_vscode/cpp/llvm/5_LoopCode/aflgo
 
 if [ -d "temp/" ]; then
     rm -r temp
 fi
 
-mkdir temp; cp LoopBBs.txt temp/
+mkdir temp
 
 cd ..
-export SUBJECT=$PWD; export TMP_DIR=$PWD/aflgo/temp
-export CC=$AFLGO/afl-clang-fast; export CXX=$AFLGO/afl-clang-fast++
+export SUBJECT=$PWD
+export TMP_DIR=$PWD/aflgo/temp
+export CC=$AFLGO/afl-clang-fast
+export CXX=$AFLGO/afl-clang-fast++
 export LDFLAGS=-lpthread
 export ADDITIONAL="-targets=$TMP_DIR/BBtargets.txt -outdir=$TMP_DIR -flto -fuse-ld=gold -Wl,-plugin-opt=save-temps"
 
-echo $'loop.c:16' > $TMP_DIR/BBtargets.txt
+echo $'loop.c:16' >$TMP_DIR/BBtargets.txt
 $CC loop.c $ADDITIONAL -ldl -g -o loop.out
+cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq >$TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
+cat $TMP_DIR/BBcalls.txt | sort | uniq >$TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
+cat $TMP_DIR/LoopBBs.txt | rev | cut -d: -f2- | rev | sort | uniq >$TMP_DIR/LoopBBs2.txt && mv $TMP_DIR/LoopBBs2.txt $TMP_DIR/LoopBBs.txt
+
+cp $TMP_DIR/LoopBBs.txt $TMP_DIR/BBtargets.txt
+
 $AFLGO/scripts/genDistance.sh $SUBJECT $TMP_DIR loop.out
 $CC loop.c -loopfile=$TMP_DIR/LoopBBs.txt -distance=$TMP_DIR/distance.cfg.txt -ldl -g -o loop.out
 cd aflgo/temp; mkdir in; echo "" > in/in
