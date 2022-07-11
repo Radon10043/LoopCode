5651d07ce9f6df6c934dacf4fc611a4aaf2ce4ad
lowry <594897710@qq.com>
2022-07-11 16:47:07 +0800

    add keep_showmap_thread.py

--- machine_learning_module/src/real_time_data.py
+++ machine_learning_module/src/real_time_data.py
@@ -13,9 +13,13 @@ i = 0
 
 
 def recordData():
-    totalTime = 12
-    sleep(2)
-    file = '/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/fuzzer_stats'
+    totalTime = 3 * 60
+    sleep(3)
+    # libming
+    file = '/home/lowry/Documents/LoopCode/scripts/LOOP/obj-loop/out/fuzzer_stats'
+    # libxml
+    # file = '/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/fuzzer_stats'
+    # jasper
     # file = '/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out/fuzzer_stats'
     # file = '/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out_afl_origin/fuzzer_stats'
     x_axis_data = [0]   # x轴数据：时间
@@ -23,18 +27,26 @@ def recordData():
     global i
     # 字典中的key值即为csv中列名
     dataframe = pd.DataFrame({'时间': x_axis_data, '覆盖路径数量': y_axis_data})
-    dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/graphData.csv", index=False)
+    # libming
+    dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/LOOP/obj-loop/out/graphData.csv", index=False)
+    # libxml
+    # dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/graphData.csv", index=False)
+    # jasper
     # dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out/graphData.csv", index=False)
     while i < totalTime:
-        sleep(1800)    # 1800s/30min读取一次数据
-        i += 0.5
+        sleep(60)    # 1800s/30min读取一次数据
+        i += 1
         with open(file) as f:
             num = int(f.readlines()[6].split(':')[1])
         x_axis_data.append(i)
         y_axis_data.append(num)
         dataframe = pd.DataFrame({'时间': x_axis_data, '覆盖路径数量': y_axis_data})
         # 将DataFrame存储为csv,index表示是否显示行名，default=True
-        dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/graphData.csv", index=False)
+        # libming
+        dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/LOOP/obj-loop/out/graphData.csv", index=False)
+        # libxml
+        # dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/graphData.csv", index=False)
+        # jasper
         # dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out/graphData.csv", index=False)
         # dataframe.to_csv("/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out_afl_origin/graphData.csv", index=False)
 
@@ -44,10 +56,14 @@ def recordData():
     plt.figure(figsize=(8, 6))  # 定义图的大小
     plt.xlabel("time")  # X轴标签
     plt.ylabel("totalPath")  # Y轴坐标标签
-    plt.title("record half an hour")  # 曲线图的标题
+    plt.title("record per min")  # 曲线图的标题
 
     plt.plot(x_axis_data, y_axis_data)  # 绘制曲线图
-    plt.savefig('/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/path_pic.jpg')  # 保存该图片
+    # libming
+    plt.savefig('/home/lowry/Documents/LoopCode/scripts/LOOP/obj-loop/out/path_pic.jpg')  # 保存该图片
+    # libxml
+    # plt.savefig('/home/lowry/Documents/LoopCode/scripts/libxml2-2.9.14/obj-afl/out1/path_pic.jpg')  # 保存该图片
+    # jasper
     # plt.savefig('/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out/path_pic.jpg')  # 保存该图片
     # plt.savefig('/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out_afl_origin/path_pic.jpg')  # 保存该图片
     plt.show()
