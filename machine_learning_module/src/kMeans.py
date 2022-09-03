5035be43a853b7efeb6f6796595ee5522dd95cba
lowry <594897710@qq.com>
2022-09-03 16:41:27 +0800

    add k_means_main.py

--- machine_learning_module/src/kMeans.py
+++ machine_learning_module/src/kMeans.py
@@ -6,8 +6,11 @@
 
 import os
 import random
+from time import sleep
+
 from sklearn.cluster import KMeans
 import numpy as np
+import loguru
 
 
 def k_means(X, y):
@@ -17,13 +20,20 @@ def k_means(X, y):
     :param y: 标签
     :return: 模型
     """
-    print('X.shape')
-    print(X.shape)
-    print('y.shape')
-    print(y.shape)
+    # print('X.shape')
+    # print(X.shape)
+    # print('y.shape')
+    # print(y.shape)
+    loguru.logger.info('X.shape')
+    loguru.logger.info(X.shape)
+    loguru.logger.info('y.shape')
+    loguru.logger.info(y.shape)
 
-    # 设置为2个聚类中心
-    Kmeans = KMeans(n_clusters=2)
+    # 设置n_clusters为聚类中心数量
+    n_clusters = int(X.shape[0] / 3)
+    # n_clusters = 5
+    loguru.logger.info(f'本次训练--->簇的数量：{n_clusters}')
+    Kmeans = KMeans(n_clusters=n_clusters)
     # 训练模型
     Kmeans.fit(X)
     return Kmeans
@@ -57,8 +67,8 @@ def write_seed_name(seed_name, path):
     :param seed_name: 种子文件名列表
     :param path: 绝对路径
     """
-    path = r'C:\Users\59489\PycharmProjects\聚类算法'
-    f = open(path + "/good_seeds.txt", "a")
+    # path = r'C:\Users\59489\PycharmProjects\聚类算法'
+    f = open(path, "a")
     for s in seed_name:
         f.write(s + '\n')
     f.close()
@@ -74,13 +84,17 @@ def choose_seed(seed_list, label_list):
     new_seed_list = []
     label_array = np.array(label_list)  # 转换成数组
     label_set = list(set(label_list))    # 得到标签集合，在转换为列表
+    loguru.logger.info(f'标签集合：{label_set}')
     for s in label_set:     # 遍历所有label集合，每次随机得到一个标签的种子文件名，并添加至new_seed_list
         index_list = np.where(label_array == s)[0]     # 该标签在label_list中的位置
-        print(f'标签{s}索引')
-        print(index_list)
+        # print(f'标签{s}索引')
+        # print(index_list)
+        loguru.logger.info(f'标签{s}索引')
+        loguru.logger.info(index_list)
         i = random.choice(index_list)       # 随机选择标签中的一个位置
         new_seed = seed_list[int(i)]     # 获取该位置的种子文件名
-        print(f'选择的种子文件名是：{new_seed}')
+        # print(f'选择的种子文件名：{new_seed}')
+        loguru.logger.info(f'选择的种子文件名：{new_seed}')
         new_seed_list.append(new_seed)      # 将该簇中的该种子添加至new_seed_list
     return new_seed_list
 
@@ -91,33 +105,36 @@ def k_means_main(seed_path=None, out_path=None):
     2. 将内容转换为二进制
     3. 将二进制数据作为特征值放入k-means求解
     4. 获得特征标签
-    5. 从每个簇种随机选择种子，并将选出的种子文件名存入文件seed_name中
+    5. 从每个簇种随机选择种子，并将选出的种子文件名存入seed_name中
     6. 将选择的种子文件名写入good_seeds.txt
     :param seed_path: 初始种子目录绝对路径
     :param out_path: 选中种子文件名存储路径
     :return: void
     """
+    sleep(60)
     X = []  # 用于存储特征值
     X_file_name = []    # 用于存储被选出的种子文件名
     y = []  # 用于存储初始标签，暂时没想好有啥可以用的，默认为空列表
     """1. 读取种子文件 & 2. 将内容转换为二进制"""
+    seed_path = seed_path + '/queue'
     files = os.listdir(seed_path)
     for file in files:  # 遍历文件夹
-        if not os.path.isdir(file) and not file.endswith('.txt'):  # 判断是否是文件夹，不是文件夹才打开。并且不以.txt结尾
+        if (not os.path.isdir(file)) and (not file.endswith('.txt')) and (file != '.state'):  # 判断是否是文件夹，不是文件夹才打开,且不以.txt结尾
+            print(file)
             X_file_name.append(file)    # 存储所有种子文件名
             x_data = get_bits(path=seed_path + '/' + file)
             X.append(x_data[0])
     X = np.array(X)
     y = np.array(y)
 
-    """3. k-means求解"""
+    """3. 将二进制数据作为特征值放入kk-means求解"""
     km = k_means(X, y)
 
     """4. 获得特征标签"""
     cc = km.cluster_centers_    # 簇中心点
     lb = km.labels_     # 特征标签
 
-    """5. 从每个簇中随机选择种子，并将选出的种子文件名存入文件seeds_name中"""
+    """5. 从每个簇中随机选择种子，并将选出的种子文件名存入seeds_name中"""
     seed_name = choose_seed(seed_list=X_file_name, label_list=lb)
 
     """6. 将选择的种子文件名写入good_seeds.txt"""
@@ -126,4 +143,5 @@ def k_means_main(seed_path=None, out_path=None):
     write_seed_name(seed_name=seed_name, path=out_path)
 
 
-k_means_main(r'C:\Users\59489\PycharmProjects\聚类算法\queue', r'C:\Users\59489\PycharmProjects\聚类算法\good_seeds.txt')
+# k_means_main(seed_path=r'/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/out',
+#              out_path=r'/home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/temp/good_seeds.txt')
---
+++ scripts/jasper-afl-lowry-kmeans.sh
@@ -0,0 +1,74 @@
+# Jasper
+rm -rf SRC
+rm -rf jasper-3.0.3
+rm -rf jasper-3.0.3-gcc
+rm -rf obj-loop
+proxychains git clone https://github.com/mdadams/jasper.git SRC
+echo "run gcc first"
+./jasper-afl-gcc-version-ya.sh
+
+cp -r SRC jasper-3.0.3
+cd jasper-3.0.3
+git checkout 020ec588 # version-3.0.3
+
+mkdir obj-loop
+mkdir obj-loop/temp
+
+# AFL path
+export AFL=/home/lowry/Documents/LoopCode/afl-lowry
+export SUBJECT=$PWD
+export TMP_DIR=$PWD/obj-loop/temp
+export PY_OUTPUT_DIR_PATH=$TMP_DIR/py.out
+export CC=$AFL/afl-clang-fast
+export CXX=$AFL/afl-clang-fast++
+export LDFLAGS=-lpthread
+export ADDITIONAL="-outdir=$TMP_DIR"
+# py module path
+export PY_PATH=/home/lowry/anaconda3/bin/python
+export PY_MAIN_PATH=/home/lowry/Documents/LoopCode/machine_learning_module/src/k_means_main.py
+export KEEP_SHOWMAP_THREAD_PATH=/home/lowry/Documents/LoopCode/machine_learning_module/src/keep_showmap_thread.py
+export PY_OUTPUT_DIR_PATH=$PWD/obj-loop/temp/py.log
+export GOOD_SEEDS_PATH=$PWD/obj-loop/temp/good_seeds.txt
+# 1st build
+cd obj-loop
+CFLAGS="$ADDITIONAL" CXXFLAGS="$ADDITIONAL" cmake ..
+make clean all
+
+# Get max line
+line=$(wc -l $TMP_DIR/BBFile.txt | cut -d ' ' -f 1)
+
+cd $SUBJECT
+mkdir obj-loop2
+cp -r obj-loop/temp obj-loop2/temp
+rm -rf obj-loop
+mv obj-loop2 obj-loop
+cd obj-loop
+
+# 2nd build
+export ADDITIONAL="-bbfile=$TMP_DIR/BBFile.txt"
+CFLAGS="$ADDITIONAL" CXXFLAGS="$ADDITIONAL" cmake ..
+make clean all
+
+mkdir in
+cp $AFL/testcases/images/jp2/not_kitty.jp2 in/
+#echo "" >in/in.jp2
+
+# echo core >/proc/sys/kernel/core_pattern
+
+# 第一次afl，用于生成模型的初始测试用例
+#$AFL/afl-fuzz -k 1 -l $line -m none -i in -o /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/$PRE_TRAIN_AFL_OUT_DIR_NAME /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/src/app/jasper --output /tmp/out_afl_origin.jpg --input @@
+# 第一次py，预训练模型
+#$PY_PATH -u $PY_MAIN_PATH --log-path $PY_OUTPUT_DIR_PATH --pre-train --model-save-path $MODEL_PATH --pre-train-testcase /home/lowry/Documents/LoopCode/scripts/jasper_in2 --gcc-version-bin /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3-gcc/obj-loop/src/app/jasper --append-args "--output /tmp/out_afl_origin.jpg --input"
+## 正式运行afl-model
+#$PY_PATH -u $PY_MAIN_PATH --log-path $PY_OUTPUT_DIR_PATH --good-seeds-path $GOOD_SEEDS_PATH --skip-log-stdout --model-load-path $MODEL_PATH --gcc-version-bin /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3-gcc/obj-loop/src/app/jasper --append-args "--output /tmp/out_afl_origin.jpg --input" --testcase-dir-path $SUBJECT/obj-loop/out & # 后台运行py
+#sleep 5s
+#$AFL/afl-fuzz -p -y -k 180 -l $line -t 1000+ -e 0 -m none -i /home/lowry/Documents/LoopCode/scripts/jasper_in2 -o $SUBJECT/obj-loop/out $SUBJECT/obj-loop/src/app/jasper --output /tmp/out.jpg --input @@
+
+# 独立运行原版afl
+#$AFL/afl-fuzz -k 1 -l $line -m none -i in -o /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/$PRE_TRAIN_AFL_OUT_DIR_NAME /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3/obj-loop/src/app/jasper --output /tmp/out_afl_origin.jpg --input @@
+#$PY_PATH -u $KEEP_SHOWMAP_THREAD_PATH --log-path $PY_OUTPUT_DIR_PATH --skip-log-stdout --model-load-path $MODEL_PATH --gcc-version-bin /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3-gcc/obj-loop/src/app/jasper --append-args "--output /tmp/out_afl_origin.jpg --input" --testcase-dir-path $SUBJECT/obj-loop/out & # 后台运行py
+#sleep 5s
+#$AFL/afl-fuzz -k 180 -l $line -t 1000+ -e 0 -m none -i /home/lowry/Documents/LoopCode/scripts/jasper_in2 -o $SUBJECT/obj-loop/out $SUBJECT/obj-loop/src/app/jasper --output /tmp/out.jpg --input @@
+$PY_PATH -u $PY_MAIN_PATH --log-path $PY_OUTPUT_DIR_PATH --good-seeds-path $GOOD_SEEDS_PATH --skip-log-stdout --gcc-version-bin /home/lowry/Documents/LoopCode/scripts/jasper-3.0.3-gcc/obj-loop/src/app/jasper --append-args "--output /tmp/out_afl_origin.jpg --input" --testcase-dir-path $SUBJECT/obj-loop/out & # 后台运行py
+sleep 5s
+$AFL/afl-fuzz -k 180 -l $line -t 1000+ -e 0 -m none -i /home/lowry/Documents/LoopCode/scripts/jasper_in2 -o $SUBJECT/obj-loop/out $SUBJECT/obj-loop/src/app/jasper --output /tmp/out.jpg --input @@
